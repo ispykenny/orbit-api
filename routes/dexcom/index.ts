34b2d5cc3a9e8c088173466959dc91c13f2a2990
Kenny Krosky <kenny.krosky@gmail.com>
2025-02-22 18:27:53 -0800

    wip dexcom

--- routes/dexcom/index.ts
+++ routes/dexcom/index.ts
@@ -12,29 +12,29 @@ dexcom.get('/', (context: Context) => {
   return context.html(`<a href="${loginUrl}">Login with Dexcom</a>`);
 });
 
-dexcom.get('/token', async (context: Context) => {
-  const code = context.req.query('code');
-  const redirectUri = process.env.BASE_URL + '/dexcom/token';
-  const clientId = process.env.DEXCOM_CLIENT_ID;
-  const clientSecret = process.env.DEXCOM_SECRET;
+// dexcom.get('/token', async (context: Context) => {
+//   const code = context.req.query('code');
+//   const redirectUri = process.env.BASE_URL + '/dexcom/token';
+//   const clientId = process.env.DEXCOM_CLIENT_ID;
+//   const clientSecret = process.env.DEXCOM_SECRET;
 
-  const response = await fetch(`${DEXCOM_URL}/v2/oauth2/token`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      client_id: clientId ?? '',
-      client_secret: clientSecret ?? '',
-      code: code ?? '',
-      grant_type: 'authorization_code',
-      redirect_uri: redirectUri ?? '',
-    }),
-  });
+//   const response = await fetch(`${DEXCOM_URL}/v2/oauth2/token`, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded',
+//     },
+//     body: new URLSearchParams({
+//       client_id: clientId ?? '',
+//       client_secret: clientSecret ?? '',
+//       code: code ?? '',
+//       grant_type: 'authorization_code',
+//       redirect_uri: redirectUri ?? '',
+//     }),
+//   });
 
-  const data = await response.json();
-  return context.json(data);
-});
+//   const data = await response.json();
+//   return context.json(data);
+// });
 
 dexcom.route('/token', token);
 dexcom.route('/refresh-tokens', refreshTokens);
--- routes/dexcom/readings.ts
+++ routes/dexcom/readings.ts
@@ -4,22 +4,21 @@ const dexcom = new Hono();
 const DEXCOM_READINGS_URL = process.env.DEXCOM_BASE_URL + '/v3/users/self/egvs';
 
 dexcom.get('/', async (context: Context) => {
+  const auth = await context.req.header('Authorization');
+  const access_token = auth?.split(' ')[1];
+
   const query = new URLSearchParams({
     startDate: '2025-02-06T09:12:35',
     endDate: '2025-02-23T09:12:35',
   }).toString();
-  const { access_token } = await context.req.json();
-  console.log(access_token);
-  // const response = await fetch(`${DEXCOM_READINGS_URL}?${query}`, {
-  //   headers: {
-  //     Authorization: `Bearer ${access_token}`,
-  //   },
-  // });
-  // const data = await response.json();
-  // return context.json(data);
-  return context.json({
-    status: 'ok',
+
+  const response = await fetch(`${DEXCOM_READINGS_URL}?${query}`, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
   });
+  const data = await response.json();
+  return context.json(data);
 });
 
 export default dexcom;
